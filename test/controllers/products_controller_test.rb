39dd82b519fe240f35be3e8fb2727bf8d0881f11
Yifat Biezuner <yifatbiz12@gmail.com>
2021-01-06 14:33:28 +0200

    update test path

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get products_index_url
+    get products_path
     assert_response :success
   end
 end
